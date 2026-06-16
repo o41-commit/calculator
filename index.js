022e884ed52e2c9af0f734730e04f0675f2b86b4
o41-commit <oluwatobilobafatoba@gmail.com>
2026-06-16 12:00:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const input = document.getElementById("input");
+const result = document.getElementById("result");
+const clear = document.getElementById("clear");
+const number = document.getElementById("number");
+const equal = document.getElementById("equal");
+const multiply = document.getElementById("multiply");
+
+// Number buttons
+const numberClick = () => {
+  const numberButtons = number.querySelectorAll("button:not(#equal)");
+
+  numberButtons.forEach((item) => {
+    item.addEventListener("click", () => {
+      input.textContent += item.textContent;
+    });
+  });
+};
+
+// Operator buttons
+const signClick = () => {
+  const operatorButtons = multiply.children;
+
+  for (let item of operatorButtons) {
+    item.addEventListener("click", () => {
+      input.textContent += item.textContent;
+    });
+  }
+};
+
+// Clear button
+clear.addEventListener("click", () => {
+  input.textContent = "";
+  result.textContent = "";
+});
+
+
+
+// Equal button
+equal.addEventListener("click", () => {
+    try {
+  const answer = eval(input.textContent)
+
+  if(answer){
+    input.textContent = answer
+  }
+      result.textContent = answer  
+    } catch (error) {
+      console.error(error, "error when calculating") 
+      result.textContent = "Syntax Error" 
+    }
+});
+
+numberClick();
+signClick();
